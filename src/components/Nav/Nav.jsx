09fd80f94030fe51a9bd58fef442b7d3458efb94
Kiryl Khomchanka <dickdurex265@gmail.com>
2024-05-03 11:18:42 +0300

    added nav links

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,16 +1,29 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import styles from './Nav.module.css'
 import { Link } from 'react-router-dom'
 
 const Nav = () => {
+  const list = useRef()
+
+  useEffect(() => {
+    const listItem = list.current.childNodes
+    listItem.forEach((list) => {
+      if (window.location.href === list.firstElementChild.href) {
+        list.classList.add(styles.active)
+      } else {
+        list.classList.remove(styles.active)
+      }
+    })
+  }, [window.location.href])
+
   return (
     <nav>
-      <ul>
+      <ul ref={list}>
         <li>
-          <Link to="/">Movies</Link>
+          <Link to="/" className={styles.link}>Movies</Link>
         </li>
         <li>
-          <Link to='rated'>Rated movies</Link>
+          <Link to="/rated" className={styles.link}>Rated movies</Link>
         </li>
       </ul>
     </nav>
