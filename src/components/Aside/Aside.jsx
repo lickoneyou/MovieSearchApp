2b0424d3408829731ba85b5d7b4f5376d0d88268
Kiryl Khomchanka <dickdurex265@gmail.com>
2024-05-03 10:13:46 +0300

    add aside

--- src/components/Aside/Aside.jsx
+++ src/components/Aside/Aside.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import styles from './Aside.module.css'
+import titleImg from '../../img/titleImg.png'
+import Nav from '../Nav/Nav'
 
 const Aside = () => {
-  return <aside className={styles.aside}>Aside</aside>
+  return (
+    <aside className={styles.aside}>
+      <div className={styles.titleWrapper}>
+        <img className={styles.titleImg} src={titleImg} alt="titleImg" />
+        <h1>ArrowFlicks</h1>
+      </div>
+      <Nav />
+    </aside>
+  )
 }
 
 export default Aside
---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './Nav.module.css'
+
+const Nav = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Movies</li>
+        <li>Rated movies</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Nav
