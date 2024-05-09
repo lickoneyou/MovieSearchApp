9d1bf1d85d02969113f0b5819fb70742a59ee8e7
Kiryl Khomchanka <dickdurex265@gmail.com>
2024-05-09 14:02:58 +0300

    added api

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,6 +1,11 @@
-export async function GET() {
-  // const API = 'https://api.jikan.moe/v4/anime?q=naruto'
-  // const res = await fetch(API)
-  // const data = await res.json()
-  // return Response.json(data)
+export async function POST(req) {
+  const reqData = await req.json()
+  const APIKey = '96ff6ab3252527b9a8b7aaa21e087ff4'
+  const url = `https://api.themoviedb.org/3/${reqData}`
+  const paramsURL = new URL(req.url)
+  const searchParams = new URLSearchParams(paramsURL.search)
+  searchParams.append('api_key', APIKey)
+  const res = await fetch(url + '?' + searchParams)
+  const data = await res.json()
+  return Response.json(data)
 }
