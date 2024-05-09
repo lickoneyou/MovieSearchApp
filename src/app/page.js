130fabcdb6fbef3303c696351588562302be9a91
Kiryl Khomchanka <dickdurex265@gmail.com>
2024-05-09 20:27:36 +0300

    deleted test fn

--- src/app/page.js
+++ src/app/page.js
@@ -7,22 +7,8 @@ import { MantineProvider } from '@mantine/core'
 import './reset.css'
 import styles from './page.module.css'
 import '@mantine/core/styles.css'
-import { useEffect } from 'react'
 
 export default function Home() {
-  const fn = async () => {
-    const res = await fetch('/api/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify('/discover/movie'),
-    })
-    const data = await res.json()
-    console.log(data)
-  }
-  useEffect(() => {
-    fn()
-  }, [])
-
   return (
     <MantineProvider>
       <div className={styles.App}>
