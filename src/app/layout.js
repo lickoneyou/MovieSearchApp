83baa961d7f1aefac35ddca00a00e5689ef7b491
Kiryl Khomchanka <dickdurex265@gmail.com>
2024-05-10 19:56:46 +0300

    moved MantineProvider in layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,8 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import './reset.css'
+import { MantineProvider } from '@mantine/core'
+import '@mantine/core/styles.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +14,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <MantineProvider>{children}</MantineProvider>
+      </body>
     </html>
   )
 }
