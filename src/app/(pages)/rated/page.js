d07705c6caa2e2205d5c5f797fba5ec6ad253d19
Kiryl Khomchanka <dickdurex265@gmail.com>
2024-05-10 07:49:16 +0300

    fix rated page styles

--- src/app/(pages)/rated/page.js
+++ src/app/(pages)/rated/page.js
@@ -1,8 +1,9 @@
-import NavPanel from '@/components/NavPanel/NavPanel'
+import NavPanel from "@/components/NavPanel/NavPanel";
+import styles from '../../page.module.css'
 
 export default function Rated() {
   return (
-    <div>
+    <div className={styles.App}>
       <NavPanel />
     </div>
   )
