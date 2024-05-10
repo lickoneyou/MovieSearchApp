ef0abc99416d5eaf1e64451f76b4a0a96a370a1d
Kiryl Khomchanka <dickdurex265@gmail.com>
2024-05-10 19:59:34 +0300

    deleted MantineProvider

--- src/app/page.js
+++ src/app/page.js
@@ -3,19 +3,14 @@
 import ControlPanel from '@/components/ControlPanel/ControlPanel'
 import Films from '@/components/Films/Films'
 import NavPanel from '@/components/NavPanel/NavPanel'
-import { MantineProvider } from '@mantine/core'
-import './reset.css'
 import styles from './page.module.css'
-import '@mantine/core/styles.css'
 
 export default function Home() {
   return (
-    <MantineProvider>
-      <div className={styles.App}>
-        <NavPanel />
-        <ControlPanel />
-        <Films />
-      </div>
-    </MantineProvider>
+    <div className={styles.App}>
+      <NavPanel />
+      <ControlPanel />
+      <Films />
+    </div>
   )
 }
