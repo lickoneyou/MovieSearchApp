7bd9249f4706d34211d0cbeb32b940f71fff0693
Kiryl Khomchanka <dickdurex265@gmail.com>
2024-05-10 20:02:26 +0300

    expanded FilmCard component

--- src/components/Films/FilmCard.js
+++ src/components/Films/FilmCard.js
@@ -8,6 +8,10 @@ import getYears from '@/handlers/getYears'
 import normalizeVoteAverage from '@/handlers/normalizeVoteAverage'
 import getGenres from '@/handlers/getGenres'
 import normalizeVoteCount from '@/handlers/normalizeVoteCount'
+import normalizeGenres from '@/handlers/normalizeGenres'
+import normalizeTime from '@/handlers/normalizeTime'
+import normalizeDate from '@/handlers/normalizeDate'
+import normalizeСurrency from '@/handlers/normalizeСurrency'
 
 const FilmCard = ({
   imgSrc,
@@ -17,24 +21,31 @@ const FilmCard = ({
   voteAverage,
   voteCount,
   genreIds,
+  genresArr = [],
+  duration,
+  premiere,
+  budget,
+  grossWorldwide,
 }) => {
   const [genres, setGenres] = useState([])
 
   useEffect(() => {
-    getGenres(genreIds, setGenres)
-  }, [genreIds])
+    if (genreIds) getGenres(genreIds, setGenres)
+  }, [])
 
   return (
     <div className={styles.filmCard}>
       <div className={styles.filmCardContainer}>
         <Image
           className={styles.filmCardPoster}
-          src={getImgSrc(300, imgSrc)}
+          src={getImgSrc('400', imgSrc)}
           alt={title}
           width="0"
           height="0"
           sizes="100vw"
-          style={{ width: '100%', height: 'auto' }}
+          style={{ width: 'auto', height: '100%' }}
+          placeholder="blur"
+          blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNcVA8AAckBI7zuPHQAAAAASUVORK5CYII="
         />
         <div className={styles.filmCardDescr}>
           <div>
@@ -54,9 +65,44 @@ const FilmCard = ({
               </p>
             </div>
           </div>
-          <p className={styles.filmCardGenres}>
-            Genre <span>{genres.join(', ')}</span>
-          </p>
+          <div>
+            {duration ? (
+              <p className={styles.filmCardWriteUp}>
+                Duration <span>{normalizeTime(duration)}</span>
+              </p>
+            ) : (
+              false
+            )}
+            {premiere ? (
+              <p className={styles.filmCardWriteUp}>
+                Premiere <span>{normalizeDate(premiere)}</span>
+              </p>
+            ) : (
+              false
+            )}
+            {budget ? (
+              <p className={styles.filmCardWriteUp}>
+                Budget <span>{normalizeСurrency(budget)}</span>
+              </p>
+            ) : (
+              false
+            )}
+            {grossWorldwide ? (
+              <p className={styles.filmCardWriteUp}>
+                Gross worldwide <span>{normalizeСurrency(grossWorldwide)}</span>
+              </p>
+            ) : (
+              false
+            )}
+            <p className={styles.filmCardWriteUp}>
+              Genre{' '}
+              <span>
+                {genreIds
+                  ? genres.join(', ')
+                  : normalizeGenres(genresArr).join(', ')}
+              </span>
+            </p>
+          </div>
         </div>
       </div>
       <IconStarFilled className={styles.filmCardMyRatingStars} />
