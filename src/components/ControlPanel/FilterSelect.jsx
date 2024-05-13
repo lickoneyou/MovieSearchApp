6bc508c8a406c87affb62900d21365335f38e084
Kiryl Khomchanka <dickdurex265@gmail.com>
2024-05-13 18:19:07 +0300

    added filter logic

--- src/components/ControlPanel/FilterSelect.jsx
+++ src/components/ControlPanel/FilterSelect.jsx
@@ -1,5 +1,9 @@
+import getAllGenres from '@/handlers/getAllGenres'
+import getGenreId from '@/handlers/getGenreId'
+import getGenreName from '@/handlers/getGenreName'
 import { Group, Select, rem } from '@mantine/core'
 import { IconChevronDown, IconSelector } from '@tabler/icons-react'
+import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import styles from './FilterSelect.module.css'
 
@@ -19,7 +23,12 @@ function FilterSelect({
     <IconSelector style={{ width: rem(16), height: rem(16) }} />
   )
   const dispath = useDispatch()
-  const value = useSelector((value) => value[filter])
+  const [genre, setGenre] = useState([])
+  const value = useSelector(value => value)
+
+  useEffect(() => {
+    getAllGenres(setGenre)
+  }, [])
 
   return (
     <Group justify="space-between" mt={mt}>
@@ -30,14 +39,20 @@ function FilterSelect({
         data={data}
         radius="sm"
         size="md"
-        id=""
-        value={value}
+        value={label === 'Genres' ? getGenreName(genre, value[filter]) : value[filter]}
         placeholder={placeholder}
         label={label}
         rightSection={isIcon ? iconChevronDown : iconSelector}
-        onChange={(e) =>
-          dispath({ type: 'ADD_FILTER', payload: { filter: filter, value: e } })
-        }
+        comboboxProps={{ transitionProps: { transition: 'pop', duration: 200 }, shadow: 'md' }}
+        onChange={(e) => {
+          dispath({
+            type: 'ADD_FILTER',
+            payload: {
+              filter: filter,
+              value: label === 'Genres' ? getGenreId(genre, e) : e,
+            },
+          })
+        }}
       />
     </Group>
   )
