9019c72b3dc0eee84f9fc7a4dae7aab1a98cc26d
Kiryl Khomchanka <dickdurex265@gmail.com>
2024-05-13 18:21:50 +0300

    refactor getOldestDate fn

--- src/handlers/getOldestDate.js
+++ src/handlers/getOldestDate.js
@@ -1,10 +1,10 @@
 import getYears from './getYears'
 
 const getOldestDate = async (set) => {
-  const res = await fetch('/api?sort_by=primary_release_date.asc', {
+  const res = await fetch('/api/discover/movie?sort_by=primary_release_date.asc', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify('/discover/movie'),
+    body: JSON.stringify([]),
   })
   const data = await res.json()
   set(getYears(data.results[0].release_date))
