4e7fca89dcfd35f35919631fb508a471d33cd9b9
Kiryl Khomchanka <dickdurex265@gmail.com>
2024-05-13 18:21:15 +0300

    refactor getFilms fn

--- src/handlers/getFilms.js
+++ src/handlers/getFilms.js
@@ -1,8 +1,8 @@
-const getFilms = async (set) => {
-  const res = await fetch('/api/', {
+const getFilms = async (set, query) => {
+  const res = await fetch('/api/discover/movie', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify('/discover/movie'),
+    body: JSON.stringify(query),
   })
   const data = await res.json()
   set(data)
