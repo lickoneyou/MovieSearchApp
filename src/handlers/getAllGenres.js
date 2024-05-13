d6aa29e298582fd8610a5338e6c4bafcc941b4f1
Kiryl Khomchanka <dickdurex265@gmail.com>
2024-05-13 18:21:00 +0300

    refactor getAllGenres fn

--- src/handlers/getAllGenres.js
+++ src/handlers/getAllGenres.js
@@ -1,9 +1,5 @@
 const getAllGenres = async (set) => {
-  const res = await fetch('/api/', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify('genre/movie/list'),
-  })
+  const res = await fetch('/api/genre/movie/list')
   const data = await res.json()
   set(data.genres)
 }
