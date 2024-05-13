3bc7546ae6afe52ac56944789ca5ef704550265c
Kiryl Khomchanka <dickdurex265@gmail.com>
2024-05-13 18:20:28 +0300

    movie getting refactor

--- src/components/Films/Films.jsx
+++ src/components/Films/Films.jsx
@@ -3,13 +3,16 @@ import { useEffect, useState } from 'react'
 import Spinner from '../Spinner/Spinner'
 import styles from './Films.module.css'
 import FilmsPage from './FilmsPage'
+import { useSelector } from 'react-redux'
+import getQueryArray from '@/handlers/getQueryArray'
 
 const Films = () => {
   const [films, setFilms] = useState('')
+  const filters = useSelector((filters) => filters)
 
   useEffect(() => {
-    getFilms(setFilms)
-  }, [])
+    getFilms(setFilms, getQueryArray(filters))
+  }, [filters])
 
   return (
     <main className={styles.films}>
