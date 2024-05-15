55da7811c16914e32b7b28c5b1f55c5017b51694
Kiryl Khomchanka <dickdurex265@gmail.com>
2024-05-15 14:53:54 +0300

    added search route

---
+++ src/app/api/search/movie/route.js
@@ -0,0 +1,12 @@
+export async function POST(req) {
+  const reqData = await req.json()
+  const APIKey = '96ff6ab3252527b9a8b7aaa21e087ff4'
+  const url = `https://api.themoviedb.org/3/search/movie`
+  const paramsURL = new URL(req.url)
+  const searchParams = new URLSearchParams(paramsURL.search)
+  searchParams.append('api_key', APIKey)
+  reqData.forEach((query) => searchParams.append(query[0], query[1]))
+  const res = await fetch(url + '?' + searchParams)
+  const data = await res.json()
+  return Response.json(data)
+}
