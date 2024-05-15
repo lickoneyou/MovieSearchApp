bb9a0777cb7992877865cf0794736b671df33ccc
Kiryl Khomchanka <dickdurex265@gmail.com>
2024-05-15 15:01:05 +0300

    added '/' on res

--- src/handlers/getFilmInfo.js
+++ src/handlers/getFilmInfo.js
@@ -1,5 +1,5 @@
 const getFilmInfo = async (set, id) => {
-  const res = await fetch(`/api/movie${id}`)
+  const res = await fetch(`/api/movie/${id}`)
   const data = await res.json()
   set(data)
 }
