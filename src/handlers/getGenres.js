446e62a4fba20fb448e8870e785e0d123ff47cf9
Kiryl Khomchanka <dickdurex265@gmail.com>
2024-05-15 15:02:33 +0300

    change res

--- src/handlers/getGenres.js
+++ src/handlers/getGenres.js
@@ -1,9 +1,5 @@
 const getGenres = async (genreIds, set) => {
-  const res = await fetch('/api/', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify('genre/movie/list'),
-  })
+  const res = await fetch('/api/genre/movie/list')
   const data = await res.json()
   const result = []
   data.genres.forEach((genre) =>
