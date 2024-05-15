c13d472a2438902099749259ee56577ba6ed7f44
Kiryl Khomchanka <dickdurex265@gmail.com>
2024-05-15 14:59:26 +0300

    added click event on links

--- src/components/Links/Links.jsx
+++ src/components/Links/Links.jsx
@@ -5,25 +5,31 @@ import Link from 'next/link'
 import styles from './Links.module.css'
 import { useRouter } from 'next/navigation'
 import highlightingNavElements from '@/handlers/highlightingNavElements'
+import { useDispatch } from 'react-redux'
 
 const Links = () => {
   const list = useRef()
   const router = useRouter()
+  const dispatch = useDispatch()
 
   useEffect(() => {
     highlightingNavElements(list)
   }, [router])
 
+  const deletedFilters = () => {
+    dispatch({ type: 'RESET_FILTERS' })
+  }
+
   return (
     <nav className={styles.nav}>
       <ul ref={list}>
         <li>
-          <Link href="/" className={styles.link}>
+          <Link href="/" className={styles.link} onClick={deletedFilters}>
             Movies
           </Link>
         </li>
         <li>
-          <Link href="/rated" className={styles.link}>
+          <Link href="/rated" className={styles.link} onClick={deletedFilters}>
             Rated movies
           </Link>
         </li>
