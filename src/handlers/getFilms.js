b7c6d998b5a30457904e37d216caa03fb6c2a5f8
Kiryl Khomchanka <dickdurex265@gmail.com>
2024-05-17 19:56:45 +0300

    added trycatch block

--- src/handlers/getFilms.js
+++ src/handlers/getFilms.js
@@ -1,11 +1,16 @@
 const getFilms = async (set, query, path) => {
-  const res = await fetch(`/api/${path}/movie`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(query),
-  })
-  const data = await res.json()
-  set(data)
+  try {
+    const res = await fetch(`/api/${path}/movie`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(query),
+    })
+    const data = await res.json()
+    set(data)
+    return data
+  } catch (error) {
+    set('Server')
+  }
 }
 
 export default getFilms
--- src/handlers/getOldestDate.js
+++ src/handlers/getOldestDate.js
@@ -1,13 +1,20 @@
 import getYears from './getYears'
 
 const getOldestDate = async (set) => {
-  const res = await fetch('/api/discover/movie?sort_by=primary_release_date.asc', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify([]),
-  })
-  const data = await res.json()
-  set(getYears(data.results[0].release_date))
+  try {
+    const res = await fetch(
+      '/api/discover/movie?sort_by=primary_release_date.asc',
+      {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify([]),
+      },
+    )
+    const data = await res.json()
+    set(getYears(data.results[0].release_date))
+  } catch (error) {
+    return set('Server')
+  }
 }
 
 export default getOldestDate
