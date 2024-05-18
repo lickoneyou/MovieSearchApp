b9e7ada7f11a2da3615fdbcfaa17ac716f0dc8c6
Kiryl Khomchanka <dickdurex265@gmail.com>
2024-05-18 13:52:26 +0300

    added ratedFilms page

--- src/app/(pages)/rated/page.js
+++ src/app/(pages)/rated/page.js
@@ -1,10 +1,40 @@
+'use client'
+
+import FilmCard from '@/components/Films/FilmCard'
+import FilmsPage from '@/components/Films/FilmsPage'
 import NavPanel from '@/components/NavPanel/NavPanel'
-import styles from '../../page.module.css'
+import { useSelector } from 'react-redux'
+import styles from './ratedFilms.module.css'
 
 export default function Rated() {
+  const storageData = useSelector((data) => data.ratedData)
+  const films = Object.values(storageData)
+
   return (
     <div className={styles.App}>
       <NavPanel />
+      <div className={styles.ratedPageWrapper}>
+        <h2 className={styles.ratedFilmsTitle}>Watched movies</h2>
+        <div className={styles.filmsPage}>
+          {films ? (
+            films.map((film) => (
+              <FilmCard
+                key={film.id}
+                imgSrc={film.imgSrc}
+                title={film.title}
+                id={film.id}
+                releaseDate={film.releaseDate}
+                voteAverage={film.voteAverage}
+                voteCount={film.voteCount}
+                genreIds={film.genreIds}
+                rValue={film.rValue}
+              />
+            ))
+          ) : (
+            <div></div>
+          )}
+        </div>
+      </div>
     </div>
   )
 }
