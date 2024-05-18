93aa90f52d17d29b8398c8c1d95aa1f2ca73a44c
Kiryl Khomchanka <dickdurex265@gmail.com>
2024-05-18 17:15:59 +0300

    added 404 page

--- src/app/(pages)/[filmId]/page.js
+++ src/app/(pages)/[filmId]/page.js
@@ -1,12 +1,41 @@
+'use client'
+
 import FilmFullInfo from '@/components/Films/FilmFullInfo'
 import NavPanel from '@/components/NavPanel/NavPanel'
-import styles from '../../page.module.css'
+import { usePathname, useRouter } from 'next/navigation'
+import img from '../../../img/404.png'
+import Image from 'next/image'
+import { Button } from '@mantine/core'
+import styles from './404.module.css'
 
 export default function FilmPage() {
+  const pathname = usePathname().replace(/[//]/gi, '')
+  const router = useRouter()
+
   return (
     <div className={styles.App}>
-      <NavPanel />
-      <FilmFullInfo />
+      {isFinite(pathname) ? (
+        <>
+          <NavPanel />
+          <FilmFullInfo />
+        </>
+      ) : (
+        <>
+          <NavPanel is404={true} />
+          <div className={styles.notFoundWrapper}>
+            <Image src={img} alt="404" />
+            <h3 className={styles.notFoundTitle}>
+              We can’t find the page you are looking for
+            </h3>
+            <Button
+              className={styles.notFoundBtn}
+              onClick={() => router.push('/')}
+            >
+              Go Home
+            </Button>
+          </div>
+        </>
+      )}
     </div>
   )
 }
--- src/components/NavPanel/NavPanel.jsx
+++ src/components/NavPanel/NavPanel.jsx
@@ -3,14 +3,16 @@ import titleImg from '../../img/titleImg.png'
 import Image from 'next/image'
 import Links from '../Links/Links'
 
-const NavPanel = () => {
+const NavPanel = ({ is404 = false }) => {
+  const bg = is404 ? { background: 'transparent' } : {}
+
   return (
-    <aside className={styles.aside}>
+    <aside className={styles.aside} style={bg}>
       <div className={styles.titleWrapper}>
-        <Image className={styles.titleImg} src={titleImg} alt='titleImg'/>
+        <Image className={styles.titleImg} src={titleImg} alt="titleImg" />
         <h1>ArrowFlicks</h1>
       </div>
-      <Links />
+      {is404 ? <></> : <Links />}
     </aside>
   )
 }
