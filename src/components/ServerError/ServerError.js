8ea14eaa6c3b5f0f989e4db5f3bc7cbd79fdd9b6
Kiryl Khomchanka <dickdurex265@gmail.com>
2024-05-19 13:35:14 +0300

    fix ServerError component

--- src/components/ServerError/ServerError.js
+++ src/components/ServerError/ServerError.js
@@ -1,15 +1,15 @@
 import { Text, Title, TextInput, Button, Image } from '@mantine/core'
 import { useRouter } from 'next/navigation'
 import image from '../../img/500.png'
-import classes from './ServerError.module.css'
+import styles from './ServerError.module.css'
 
 const ServerError = () => {
   const router = useRouter()
 
   return (
-    <div className={classes.wrapper}>
-      <div className={classes.body}>
-        <Title className={classes.title}>500. That's an error.</Title>
+    <div className={styles.wrapper}>
+      <div className={styles.body}>
+        <Title className={styles.title}>500. That's an error.</Title>
         <Text fw={500} fz="lg" mb={5}>
           The server encountered an error and could not complete your request
         </Text>
@@ -17,13 +17,13 @@ const ServerError = () => {
           Please enable VPN and try again
         </Text>
 
-        <div className={classes.controls}>
-          <Button className={classes.control} onClick={() => router.refresh()}>
+        <div className={styles.controls}>
+          <Button className={styles.control} onClick={() => router.refresh()}>
             Try again
           </Button>
         </div>
       </div>
-      <Image src={image.src} className={classes.image} />
+      <Image src={image.src} className={styles.image} />
     </div>
   )
 }
