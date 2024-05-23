f0a201ada5ff80a7e0cf2ab720e51424acd1fb36
Kiryl Khomchanka <dickdurex265@gmail.com>
2024-05-23 12:14:45 +0300

    dev suspense

--- src/app/(pages)/search/page.js
+++ src/app/(pages)/search/page.js
@@ -3,17 +3,14 @@
 import ControlPanel from '@/components/ControlPanel/ControlPanel'
 import Films from '@/components/Films/Films'
 import NavPanel from '@/components/NavPanel/NavPanel'
-import { Suspense } from 'react'
 import styles from '../../page.module.css'
 
 export default function Search() {
   return (
-    <Suspense fallback={<div>q123</div>}>
-      <div className={styles.App}>
-        // <NavPanel />
-        // <ControlPanel />
-        // <Films path="search" />
-      </div>
-    </Suspense>
+    <div className={styles.App}>
+      // <NavPanel />
+      // <ControlPanel />
+      // <Films path="search" />
+    </div>
   )
 }
